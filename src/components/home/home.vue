fdade7b225811a998a60926dba89aa071ac56234
Elf-Lucia <1774697865@qq.com>
2020-11-01 17:25:18 +0800

    调整侧边栏引用样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,10 +1,130 @@
 <template>
-  <div>home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4"><img src="../../assets/logo.png" alt="" /> </el-col>
+        <el-col :span="16"><h2>电商管理平台</h2></el-col>
+        <el-col :span="4" class="exit"><a href="javascript:;">退出</a> </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside">
+        <!-- 侧边栏导航el-menu -->
+        <el-menu
+          :unique-opened="false"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">用户管理</span>
+            </template>
+            <el-menu-item index="1-4">
+              <i class="el-icon-location"></i>
+              <span slot="title">用户列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 权限管理 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-user-solid"></i>
+              <span slot="title">权限管理</span>
+            </template>
+            <el-menu-item index="2-1">
+              <i class="el-icon-location"></i>
+              <span slot="title">权限列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 商品管理 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span slot="title">商品管理</span>
+            </template>
+            <el-menu-item index="3-1">
+              <i class="el-icon-location"></i>
+              <span slot="title">商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="3-2">
+              <i class="el-icon-location"></i>
+              <span slot="title">分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="3-3">
+              <i class="el-icon-location"></i>
+              <span slot="title">商品分类</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 订单管理 -->
+           <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-s-help"></i>
+              <span slot="title">订单管理</span>
+            </template>
+            <el-menu-item index="4-1">
+              <i class="el-icon-location"></i>
+              <span slot="title">订单列表</span>
+            </el-menu-item>
+          </el-submenu>
+          <!-- 数据统计 -->
+           <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-s-data"></i>
+              <span slot="title">数据统计</span>
+            </template>
+            <el-menu-item index="4-1">
+              <i class="el-icon-location"></i>
+              <span slot="title">数据报表</span>
+            </el-menu-item>
+           
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
 
 <style scoped>
+.container {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+.header {
+  background-color: #b3c0d1;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+}
+.header img {
+  text-align: left;
+  vertical-align: middle;
+  height: 60px;
+}
+.header .exit {
+  text-align: right;
+  text-decoration: none;
+  color: black;
+}
+.aside {
+  width: 200px;
+  background-color: #d3dce6;
+}
+.main {
+  background-color: #e9eef3;
+}
 </style>
